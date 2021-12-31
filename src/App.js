88165ccf17ee08071f74a47d4f1caaf3cd8ff646
Allen Pottinger <allenpottinger@gmail.com>
2021-12-31 18:11:12 -0500

    adds bg image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Box, CssBaseline } from '@mui/material';
+import backie from './images/brooks.jpg';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Box sx={{
+      backgroundImage: `url(${backie})`,
+      backgroundSize: 'cover',
+      height: '100vh',
+      backgroundRepeat: 'no-repeat'
+    }}>
+      <CssBaseline />
+
+    </Box>
+    
   );
 }
 
