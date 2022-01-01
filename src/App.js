0834eb6075bc1df4f82deeb33ea68f4c451c64e0
Allen Pottinger <allenpottinger@gmail.com>
2021-12-31 20:21:30 -0500

    refactors styles to useStyles hook

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import { Box, CssBaseline } from '@mui/material';
 import backie from './images/brooks.jpg';
+import useStyles from './styles';
+import Navbar from './components/Navbar/Navbar';
+/*import Body from './components/Body/Body';
+import Footer from './components/Footer/Footer'; */
 
 function App() {
+
+  const classes = useStyles();
+
   return (
-    <Box sx={{
+    <Box className={classes.main} sx={{
       backgroundImage: `url(${backie})`,
       backgroundSize: 'cover',
       height: '100vh',
       backgroundRepeat: 'no-repeat'
     }}>
       <CssBaseline />
-
+      <Navbar />
+      {/* <Body />
+      <Footer /> */}
     </Box>
-    
   );
 }
 
