e9b47d825b1b7bfc3f6fd92acd9e6056624c4e6d
Allen Pottinger <allenpottinger@gmail.com>
2022-01-02 14:33:22 -0500

    styles for Navbar

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -3,6 +3,11 @@ import { makeStyles } from '@mui/styles';
 const useStyles = makeStyles({
     title: {
         letterSpacing: '1rem'
+    },
+    grid: {
+        paddingTop: '2rem',
+        display: 'flex',
+        justifyContent: 'space-around',
     }
 })
 
