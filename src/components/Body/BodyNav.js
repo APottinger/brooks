0c9693249a231c1d59e8154dcd1bd774cef4a3e8
Allen Pottinger <allenpottinger@gmail.com>
2022-01-04 11:26:32 -0500

    refactors nav items to map function

--- src/components/Body/BodyNav.js
+++ src/components/Body/BodyNav.js
@@ -1,45 +1,41 @@
 import { Typography, ThemeProvider, Grid } from '@mui/material';
-import { createTheme } from '@mui/material/styles';
 import { makeStyles } from '@mui/styles';
+import theme from '../../helpers/ThemeProvider';
 
 const BodyNav = () => {
 
-    const font = createTheme({
-        typography: {
-            fontFamily: 'Island Moments, cursive'
-        }
-    });
-
     const useStyles = makeStyles({
-        nav: {
+        grid: {
+            height: '70vh',
             display: 'flex',
-            width: '100%',
-            justifyContent: 'space-around',
-            
+            paddingLeft: '50px',
+            justifyContent: 'center',
+            alignItems: 'center'
+        },
+        gridItem: {
+            fontSize: '10rem',
+            padding: '2rem',
+            cursor: 'pointer',
+            transition: '1s ease',
+            '&:hover': {
+                color: '#b59c82'
+            }
         }
     });
 
     const classes = useStyles();
 
     return (
-        <div className={classes.nav}>
-            <ThemeProvider theme={font}>
-                <Grid container className={classes.nav}>
-                    <Grid item>
-                            <Typography variant='h1' color='#eeeef0'>
-                                <em>eat</em>
+        <div>
+            <ThemeProvider theme={theme}>
+                <Grid container className={classes.grid}>
+                    {['EAT', 'DRINK', 'VISIT'].map(item => (
+                        <Grid item key={item}>
+                            <Typography className={classes.gridItem} variant='h1' color='#eeeef0'>
+                                {item}
                             </Typography>
-                    </Grid>
-                    <Grid item>
-                            <Typography variant='h1' color='#eeeef0'>
-                                <em>drink</em>
-                            </Typography>
-                    </Grid>
-                    <Grid item>
-                            <Typography variant='h1' color='#eeeef0'>
-                                <em>visit</em>
-                            </Typography>
-                    </Grid>
+                        </Grid>
+                    ))}
                 </Grid>
             </ThemeProvider>
         </div>
