18dfafc841396ba68c1d8ffc5c81ef13475b6535
Allen Pottinger <allenpottinger@gmail.com>
2022-01-04 11:26:52 -0500

    refactors styles

--- src/App.js
+++ src/App.js
@@ -1,44 +1,40 @@
 import { Box, CssBaseline, Collapse } from '@mui/material';
 import { useEffect, useState } from 'react';
-import backie from './images/brooks.jpg';
+import backie from './images/coffee.jpg';
 import Navbar from './components/Navbar/Navbar';
 import Body from './components/Body/Body';
+import BodyNav from './components/Body/BodyNav';
 /* import Footer from './components/Footer/Footer'; */
+import useStyles from './styles';
 
 function App() {
 
+  //styling
+    const classes = useStyles();
+
+    //parallax
     const [checked, setChecked] = useState(false);
   
     useEffect(() => {
-      debugger
       setChecked(true);
     }, []);
 
   return (
-    <Box sx={{position: 'relative', overflow: 'hidden', backgroundColor: '#445756'}} >
-      <Box sx={{
-        position: 'inherit',
-        backgroundImage: `url(${backie})`,
-        backgroundSize: 'cover',
-        height: '105vh',
-        backgroundRepeat: 'no-repeat',
-        /* transition: 'height 3s',
-        '&:hover': {
-          height: '100vh'
-        } */
-      }}> 
-        <CssBaseline />
+    <Box className={classes.box} >
+      <CssBaseline />
+      <Box className={classes.boxItem}>
+        <Box sx={{width: 'calc(100% + 40px)'}}>
         <Collapse
         in={checked}
         {...(checked ? { timeout: 1000 } : {})}
-        collapsedHeight={50}
-      >
-        <Navbar />
+        collapsedHeight={50}>
+          <Navbar />
         </Collapse>
         <Body />
-        {/* <Footer /> */}
+        <BodyNav />
+        </Box>
       </Box>
-      
+    
     </Box>
   );
 }
