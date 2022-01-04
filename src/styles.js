cc994e21018cb290a582d0c4b1ecb53bd0147894
Allen Pottinger <allenpottinger@gmail.com>
2022-01-04 11:27:07 -0500

    adds App mui  styles

---
+++ src/styles.js
@@ -0,0 +1,20 @@
+import { makeStyles } from '@mui/styles';
+import backie from './images/coffee.jpg';
+
+const useStyles = makeStyles({
+    box: {
+        position: 'relative',
+        overflow: 'hidden',
+        backgroundColor: '#b59c82'
+    },
+    boxItem: {
+        position: 'inherit',
+        right: '40px',
+        backgroundImage: `url(${backie})`,
+        backgroundSize: 'cover',
+        height: '100vh',
+        backgroundRepeat: 'no-repeat'
+    }
+});
+
+export default useStyles;
