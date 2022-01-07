c76f462d0c7ef67d704c923df085aaf67bd70405
Allen Pottinger <allenpottinger@gmail.com>
2022-01-07 11:21:07 -0500

    refactors App Compnent

--- src/App.js
+++ src/App.js
@@ -1,41 +1,25 @@
-import { Box, CssBaseline, Collapse } from '@mui/material';
-import { useEffect, useState } from 'react';
-import backie from './images/coffee.jpg';
+import { Box, CssBaseline } from '@mui/material';
 import Navbar from './components/Navbar/Navbar';
-import Body from './components/Body/Body';
+import BodyAddress from './components/Body/BodyAddress';
 import BodyNav from './components/Body/BodyNav';
 /* import Footer from './components/Footer/Footer'; */
 import useStyles from './styles';
 
 function App() {
 
-  //styling
     const classes = useStyles();
 
-    //parallax
-    const [checked, setChecked] = useState(false);
-  
-    useEffect(() => {
-      setChecked(true);
-    }, []);
-
   return (
-    <Box className={classes.box} >
+  
+      <Box className={classes.box}>
       <CssBaseline />
-      <Box className={classes.boxItem}>
-        <Box sx={{width: 'calc(100% + 40px)'}}>
-        <Collapse
-        in={checked}
-        {...(checked ? { timeout: 1000 } : {})}
-        collapsedHeight={50}>
+        <Box className={classes.boxItem}>
           <Navbar />
-        </Collapse>
-        <Body />
-        <BodyNav />
+          <BodyAddress />
+          <BodyNav />
         </Box>
       </Box>
-    
-    </Box>
+
   );
 }
 
