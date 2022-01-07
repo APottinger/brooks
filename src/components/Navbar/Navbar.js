2aed3dc2a2195b46a233fecf9e523590d96b7b8e
Allen Pottinger <allenpottinger@gmail.com>
2022-01-07 11:21:28 -0500

    refactors styles

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
-import { Typography, Grid } from '@mui/material';
+import { Typography, Grid, Collapse } from '@mui/material';
+import { useEffect, useState } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import useStyles from './styles';
 import theme from '../../helpers/ThemeProvider'
@@ -6,18 +7,29 @@ import theme from '../../helpers/ThemeProvider'
 const Navbar = () => {
     
     const classes = useStyles();
+
+    const [checked, setChecked] = useState(false);
+  
+    useEffect(() => {
+      setChecked(true);
+    }, []);
    
     return (
-        <ThemeProvider theme={theme}>
-            <Grid container className={classes.grid}>
-                <Grid item>
-                    <Typography className={classes.title} color='#eeeef0' variant='h5'>
-                        bäche
-                    </Typography>
+        <Collapse
+        in={checked}
+        {...(checked ? { timeout: 1000 } : {})}
+        collapsedHeight={50}>
+            <ThemeProvider theme={theme}>
+                <Grid container className={classes.grid}>
+                    <Grid item>
+                        <Typography className={classes.title} color='#eeeef0' variant='h5'>
+                            bäche
+                        </Typography>
+                    </Grid>
+                    
                 </Grid>
-                
-            </Grid>
-        </ThemeProvider>
+            </ThemeProvider>
+        </Collapse>
     )
 }
 
