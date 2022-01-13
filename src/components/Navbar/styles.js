bcba77477fd3cd6a5d3d2705b218e51b497d8a4c
Allen Pottinger <allenpottinger@gmail.com>
2022-01-13 12:12:10 -0500

    adds drawer/icon style

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -3,14 +3,25 @@ import { makeStyles } from '@mui/styles';
 const useStyles = makeStyles({
     grid: {
         paddingTop: '3rem',
-        paddingLeft: '7rem',
-        width: 'calc(100% + 80px)',
+        paddingLeft: '5rem',
+        width: '400px',
         display: 'flex',
-        justifyContent: 'flex-start'
+        justifyContent: 'space-between'
     },
     title: {
         position: 'sticky',
         letterSpacing: '1rem',
+    },
+    drawer: {
+        backgroundColor: '#b59c82',
+        padding: '5rem',
+        height: '100vh',
+        width: '20vw'
+    },
+    drawerIcon: {
+        position: 'relative', 
+        top: -5,
+        backgrounColor: '#b59c82'
     }
 })
 
